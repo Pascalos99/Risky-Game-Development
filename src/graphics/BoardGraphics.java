3da8e1c3118c201d8e202670b24900b2e78bd9e4
arthur <arthur.goffinet@gmail.com>
2020-09-22 16:26:20 +0200

    initial structur

--- src/graphics/BoardGraphics.java
+++ src/graphics/BoardGraphics.java
@@ -1,4 +1,20 @@
 package graphics;
 
+import gamerules.Board;
+
+import java.awt.*;
+
 public class BoardGraphics {
+    private Image image;
+    private Image[] pawnsImage;
+    private Board board;
+
+    public Board getBoard() {
+        return board;
+    }
+
+    //TODO implement the method
+    public void notifyUpdate(){
+
+    }
 }
