5d04560299dfd7c887f2d11a3bd6ac050e959902
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-09-23 19:17:33 +0200

    fixed project setup (on my end)

--- src/main/java/com/company/BoardElement.java
+++ src/main/java/com/company/BoardElement.java
@@ -1,4 +1,4 @@
-package main.java.com.company;
+package com.company;
 
 import javax.swing.*;
 import java.awt.*;
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,5 +1,3 @@
-package java;
-
 import org.junit.jupiter.api.Test;
 
 public class SampleTest {
