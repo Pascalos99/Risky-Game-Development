e34125abdd91e2599dd73da3d35d368b7cbafca1
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-09-23 15:00:48 +0200

    moved getCoordinateOfNode

--- src/gamerules/Board.java
+++ src/gamerules/Board.java
@@ -53,23 +53,6 @@ public class Board {
     	return -1;
     }
     
-    /**
-     * Gives the coordinate of a given node on the game-board. The coordinate lies within [0,1] x [0,1] and denotes the centre
-     * of the node at the Board-image used in-game (this means it will not be affected by any changes done to the board-image after BoardGraphics)
-     * @param node a boardnode
-     * @return a Point2D object representing the node's coordinate
-     * invariant: x and y must lie in the range [0,1]
-     */
-    public Point2D.Double getCoordinateOfNode(BoardNode node) {
-    	// not implemented yet
-    	// TODO
-    	Point2D.Double coord = new Point2D.Double();
-    	
-    	// invariant check function, do not remove
-    	if (coord.x < 0 || coord.x > 1 || coord.y < 0 || coord.y > 1) throw new AssertionError("Coordinate does not fall within [0,1]x[0,1]");
-    	return coord;
-    }
-    
     public List<Player> getPlayers() {
         return List.of(players);
     }
