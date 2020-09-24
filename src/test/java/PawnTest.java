9a16c522c704e5243307f8cb8db16388e39d40d7
arthur <arthur.goffinet@gmail.com>
2020-09-24 15:43:38 +0200

    Test 2

---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import gamerules.*;
+import players.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PawnTest {
+    private Player playerExample1 = new HumanPlayer();
+    private Player playerExample2 = new HumanPlayer();
+    private BoardNode node1 = new BoardNode(1,null,null);
+    private Pawn p1 = new Pawn(playerExample1,null);
+    private Pawn p2 = new Pawn(playerExample1,node1);
+
+
+    @Test
+    void testequal(){
+        assertEquals(p1!=p2,true);
+        assertEquals(p1==p1,true);
+    }
+
+    @Test
+    void testPosition1(){
+        assertEquals(p1.getPosition(),null);
+        assertEquals(p2.getPosition(),node1);
+    }
+    @Test
+    void testTostring(){
+        //TODO FIND THE BUG
+        assertEquals(p1.toString(),"(Pawn of Human-1)");
+    }
+}
