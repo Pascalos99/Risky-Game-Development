c36811fc62c63ffb6172b6da000cfcf9c8c636a6
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-10-01 16:33:10 +0200

    Revert "Merge branch 'master' of https://github.com/Pascalos99/Risky-Game-Development"
    This reverts commit 90676212d000f40547e6960d98d61725b630f70b, reversing
    changes made to a692f695c56a33b5dd141f44ba9f869a01f12d0f.

--- src/main/java/graphics/sample/Controller.java
+++ src/main/java/graphics/sample/Controller.java
@@ -1,6 +1,5 @@
 package graphics.sample;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
@@ -33,8 +32,6 @@ public class Controller implements Initializable {
     public String Name4;
     public String Name5;
     public String Name6;
-    public JFXButton PlayBtn;
-    public JFXButton GameRulesBtn;
 
     @FXML
     private JFXTextField name1;
