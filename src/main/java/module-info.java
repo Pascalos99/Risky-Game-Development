ff26a3f7cdadafb050a53492977d6b276abd5acb
Mohamad64 <49920290+Mohamad64@users.noreply.github.com>
2020-10-01 03:17:22 +0200

    jfoenix module update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,6 @@ module nl.maastrichtuniversity.dke.RiskyGameDevelopment {
     requires javafx.base;
     requires javafx.graphics;
     requires java.desktop;
-    requires com.jfoenix;
+//    requires com.jfoenix;
+    requires jfoenix;
 }
