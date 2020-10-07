3cc76e06cc2150bb9a159082ff891784c56ac794
Mohamad64 <49920290+Mohamad64@users.noreply.github.com>
2020-10-07 18:58:04 +0200

    Game Scene combining swing

--- src/main/java/graphics/sample/GameSceneController.java
+++ src/main/java/graphics/sample/GameSceneController.java
@@ -42,7 +42,7 @@ public class GameSceneController implements Initializable{
 //        JFXPanel panel = new JFXPanel();
 //        panel.setSize(500,500);
 //        panel.add(Controller.gamePanel);
-        Controller.gamePanel.setSize(500,500);
+        Controller.gamePanel.setSize(200,200);
         swingNode.setContent(Controller.gamePanel);
     }
 }
