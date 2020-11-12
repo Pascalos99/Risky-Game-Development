c3bbb4f5f07c29c54db8162e044e1b0ba66e5871
Max Persoon <persoonmax@hotmail.nl>
2020-11-12 18:30:36 +0100

    Updated GameTree

--- src/main/java/gamerules/GameTree.java
+++ src/main/java/gamerules/GameTree.java
@@ -1,5 +1,21 @@
 package gamerules;
 
+import java.util.List;
+
 public abstract class GameTree {
-	
+
+    private final GameState root;
+    private GameState current;
+
+    public GameTree(GameState root) {
+        this.root = root;
+        current = root;
+    }
+
+    private void build() {
+        for (Move move : current.getAllPossibleMoves(current.getAllPawnsOf(current.currentPlayer()))) {
+
+        }
+    }
+
 }
