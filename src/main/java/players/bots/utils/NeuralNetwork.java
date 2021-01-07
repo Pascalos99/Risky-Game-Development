8891215569fab206b60a65ad37c38708821e2886
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2021-01-07 01:10:56 +0100

    added Neural Network forward propagation

--- src/main/java/players/bots/utils/NeuralNetwork.java
+++ src/main/java/players/bots/utils/NeuralNetwork.java
@@ -1,14 +1,178 @@
 package players.bots.utils;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.function.Function;
 
 public class NeuralNetwork {
 	
+	// Testing code
+	public static void main(String[] args) {
+		int[] structure = {3, 10, 15, 8, 1};
+		Activation[] activations = {dSILU, dSILU, dSILU, LINEAR};
+		NeuralNetwork ann = new NeuralNetwork(structure, activations);
+		ann.initializeRandomWeights(-1, 1);
+		double[][][] weights = ann.getAllWeights();
+		for (int l=0; l < ann.getLayerCount(); l++)
+			Utils.printMatrix(weights[l]);
+		double[] input = {1,0,-1};
+		System.out.println("with input:");
+		Utils.printMatrix(Utils.getColumnVector(input));
+		System.out.println("generates output:");
+		Utils.printMatrix(Utils.getColumnVector(ann.forwardProp(input)));
+	}
+	
+	private Layer[] hidden_layers;
+	
+	private static boolean[] getFilled(int length) {
+		boolean[] result = new boolean[length];
+		Arrays.fill(result, true);
+		return result;
+	}
+	
+	public NeuralNetwork(int[] network_structure, Activation[] activation_functions) {
+		this(network_structure, getFilled(activation_functions.length), activation_functions);
+	}
+	
+	public NeuralNetwork(int[] network_structure, boolean[] bias_inclusion, Activation[] activation_functions) {
+		hidden_layers = new Layer[network_structure.length - 1];
+		
+		if (activation_functions.length < hidden_layers.length || bias_inclusion.length < hidden_layers.length)
+			throw new IllegalArgumentException("not enough information provided for number of layers (requires "
+					+hidden_layers.length+", got "+activation_functions.length+" and "+bias_inclusion.length+")");
+		
+		for (int a=0, b=1; b < network_structure.length; a++, b++)
+			hidden_layers[a] = new Layer(network_structure[a], network_structure[b], bias_inclusion[a], activation_functions[a]);
+	}
+	
+	/**
+	 * Initializes the weights of this neural network according to the given weight supplier.
+	 * @param weight_supplier given the integer input of layer index (starting from 0, ending at {@link #getLayerCount()}{@code -1})
+	 *  should generate a (possibly random) floating point number for some weight in that layer.
+	 */
+	public void initializeWeights(Function<Integer, Double> weight_supplier) {
+		for (int l=0; l < hidden_layers.length; l++) {
+			double[][] weights = hidden_layers[l].weights;
+			for (int i=0; i < weights.length; i++)
+				for (int j=0; j < weights[i].length; j++)
+					weights[i][j] = weight_supplier.apply(l);
+		}
+	}
+	
+	/**
+	 * Initialized the weights of this neural network with random weights between the given minimum and maximum values.
+	 * @param min the minimum weight to be generated (inclusive)
+	 * @param max the maximum weight to be generated (exclusive)
+	 * @param seed the seed of randomness for the weight generation
+	 */
+	public void initializeRandomWeights(double min, double max, long seed) {
+		Random random = new Random(seed);
+		initializeWeights(l -> random.nextDouble() * (max-min) + min);
+	}
+	
+	/**
+	 * Initialized the weights of this neural network with random weights between the given minimum and maximum values.
+	 * @param min the minimum weight to be generated (inclusive)
+	 * @param max the maximum weight to be generated (inclusive)
+	 */
+	public void initializeRandomWeights(double min, double max) {
+		initializeRandomWeights(min, max, System.currentTimeMillis());
+	}
+	
+	/**
+	 * This method allows modification of the actual weights of this neural network and should thus be used carefully
+	 * @return the weights of every layer in the network, in order of forward propagation
+	 */
+	public double[][][] getAllWeights() {
+		double[][][] result = new double[hidden_layers.length][][];
+		for (int l=0; l < result.length; l++)
+			result[l] = hidden_layers[l].getWeights();
+		return result;
+	}
+	
+	public double[] forwardProp(double[] input) {
+		int input_size = getInputSize();
+		if (input.length != input_size) throw new IllegalArgumentException
+			("input size of "+input.length+" does not match input size of layer ("+input_size+")");
+		double[] current = input;
+		for (int l=0; l < hidden_layers.length; l++)
+			current = hidden_layers[l].forwardProp(current);
+		return current;
+	}
+	
+	public int getInputSize() {
+		return hidden_layers[0].getInputSize();
+	}
+	public int getOutputSize() {
+		return hidden_layers[hidden_layers.length-1].getOutputSize();
+	}
+	public int getLayerCount() {
+		return hidden_layers.length;
+	}
+	public int[] getLayerStructure() {
+		int[] structure = new int[hidden_layers.length+1];
+		structure[0] = getInputSize();
+		for (int l=0; l < hidden_layers.length; l++)
+			structure[l+1] = hidden_layers[l].getOutputSize();
+		return structure;
+	}
+	
 	static class Layer {
+		
+		public Layer(int input_size, int output_size, boolean include_bias, Activation activation_function) {
+			bias = include_bias;
+			activation = activation_function;
+			
+			int input = input_size + (bias? 1:0);
+			weights = new double[output_size][input];
+		}
+		
+		public Layer(int input_size, int output_size, Activation activation) {
+			this(input_size, output_size, true, activation);
+		}
+		
+		private boolean bias;
 		
 		private double[][] weights;
 		
 		private Activation activation;
+		
+		public Activation getActivation() {
+			return activation;
+		}
+		
+		public double[] forwardPropRaw(double[] input) {
+			int input_size = getInputSize();
+			if (input.length != input_size) throw new IllegalArgumentException
+				("input size of "+input.length+" does not match input size of layer ("+input_size+")");
+			
+			double[] input_vector = new double[weights[0].length];
+			for (int i=0; i < input_vector.length; i++)
+				if (i==0 && bias) input_vector[i] = 1;
+				else if (bias) input_vector[i] = input[i-1];
+				else input_vector[i] = input[i];
+			
+			return Utils.extractVector(Utils.matrixVectorMul(weights, input_vector));
+		}
+		
+		public double[] forwardProp(double[] input) {
+			return activation.activate(forwardPropRaw(input));
+		}
+		
+		public int getInputSize() {
+			return weights[0].length - (bias? 1:0);
+		}
+		public int getOutputSize() {
+			return weights.length;
+		}
+		
+		/**
+		 * This method allows modification of the actual weights of this layer and should thus be used carefully
+		 * @return the weights of this layer
+		 */
+		public double[][] getWeights() {
+			return weights;
+		}
 		
 	}
 	
@@ -28,7 +192,15 @@ public class NeuralNetwork {
 		public double derivate(double x) {
 			return derivative.apply(x);
 		}
+		public double[] activate(double[] x) {
+			double[] r = new double[x.length];
+			for (int i=0; i < r.length; i++)
+				r[i] = activate(x[i]);
+			return r;
+		}
 	}
+	
+	public static final Activation LINEAR = new Activation(x -> x, x -> 1d);
 	
 	public static final Activation SIGMOID = new Activation(x -> 1 / (1 + Math.exp(-x)), x -> {
 		double f = 1 / (1 + Math.exp(-x));
@@ -53,8 +225,5 @@ public class NeuralNetwork {
 		// σ(x)(1 − σ(x))(2 + x(1 − σ(x)) − x*σ(x)) [credit to https://arxiv.org/pdf/1702.03118.pdf]
 		return s * (1 - s) * (2 + x * (1 - s) - x * s);
 	});
-	
-	private int num_inputs;
-	private Layer[] hidden_layers;
 
 }
---
+++ src/main/java/players/bots/utils/Utils.java
@@ -0,0 +1,109 @@
+package players.bots.utils;
+
+import java.util.Locale;
+
+/**
+ * Note that in all matrices, a (n*m) matrix would be represented by a double[n][m] where n denotes the height and m denotes the width<br>
+ * Distinction is also made between column vectors and row vectors, and the type may be assumed in Matrix*vector and vector*Matrix multiplication methods
+ */
+public final class Utils {
+	
+	public static void printMatrix(double[][] m) {
+		System.out.println(matrixToString(m));
+	}
+	
+	public static String matrixToString(double[][] m) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		for (int i=0; i < m.length; i++) {
+			if (i > 0) sb.append(" ");
+			sb.append("[");
+			for (int j=0; j < m[i].length; j++) {
+				sb.append(String.format(Locale.CANADA, "% .3f", m[i][j]));
+				if (j < m[i].length - 1) sb.append(", ");
+			}
+			sb.append("]");
+			if (i < m.length - 1) sb.append("\n");
+			else sb.append(")");
+		}
+		return sb.toString();
+	}
+	
+	public static double[][] transpose(double[][] m) {
+		double[][] result = new double[m[0].length][m.length];
+		for (int i=0; i < m.length; i++)
+			for (int j=0; j < m[i].length; j++)
+				result[j][i] = m[i][j];
+		return result;
+	}
+	
+	public static double[][] getColumnVector(double[] v) {
+		double[][] res = new double[v.length][1];
+		for (int i=0; i < v.length; i++)
+			res[i][0] = v[i];
+		return res;
+	}
+	public static double[][] getRowVector(double[] h) {
+		double[][] res = new double[1][h.length];
+		for (int i=0; i < h.length; i++)
+			res[0][i] = h[i];
+		return res;
+	}
+	
+	/**
+	 * @param vector must be of shape (1xN) or (Nx1)
+	 * @return the corresponding vector in array form
+	 */
+	public static double[] extractVector(double[][] vector) {
+		int height = vector.length;
+		int width = vector[0].length;
+		if (height > 1 && width > 1) throw new RuntimeException("given vector is not of shape 1xN or Nx1: ("+height+"x"+width+")");
+		if (width > height) return vector[0];
+		double[] res = new double[height];
+		for (int i=0; i < height; i++)
+			res[i] = vector[i][0];
+		return res;
+	}
+	
+	/**
+	 * @param m a matrix
+	 * @param v a vector, handled as column vector
+	 * @return resulting matrix from computing m * v
+	 */
+	public static double[][] matrixVectorMul(double[][] m, double[] v) {
+		return martixMatrixMul(m, getColumnVector(v));
+	}
+	
+	/**
+	 * @param h a vector, handled as row vector
+	 * @param m a matrix
+	 * @return resulting matrix from computing h * m
+	 */
+	public static double[][] vectorMatrixMul(double[] h, double[][] m) {
+		return martixMatrixMul(getRowVector(h), m);
+	}
+	
+	public static double[][] martixMatrixMul(double[][] a, double[][] b) {
+		// courtesy of a CS1 assignment (code by Pascal Anema)
+		int rowsA = a.length;
+		int colsA = a[0].length;
+		int colsB = b[0].length;
+		if (colsA != b.length) throw new MatrixSizeMismatchException(rowsA, colsA, b.length, colsB);
+		double[][] result = new double[rowsA][colsB];
+		for (int i=0; i < rowsA; i++)
+			for (int j=0; j < colsB; j++)
+				for (int z=0; z < colsA; z++)
+					result[i][j] += a[i][z] * b[z][j];
+		return result;
+	}
+	
+	public static class MatrixSizeMismatchException extends RuntimeException {
+		
+		private static final long serialVersionUID = -6058559419243957181L;
+
+		public MatrixSizeMismatchException(int rowsA, int colsA, int rowsB, int colsB) {
+			super("can't match matrices of shapes ("+rowsA+"x"+colsA+") and ("+rowsB+"x"+colsB+")");
+		}
+	}
+	
+}
