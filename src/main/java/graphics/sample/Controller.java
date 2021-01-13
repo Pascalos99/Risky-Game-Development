483c45a3d3f60d271af3ffacab155156c8c23f79
Mohamad64 <49920290+Mohamad64@users.noreply.github.com>
2021-01-13 22:33:59 +0000

    Random Name Generator

--- src/main/java/graphics/sample/Controller.java
+++ src/main/java/graphics/sample/Controller.java
@@ -53,7 +53,7 @@ public class Controller implements Initializable {
     public JFXButton PlayBtn;
     public JFXButton GameRulesBtn;
     public JFXButton SetNameBtn;
-    public String[] names = new String[]{"Donald Trump","Daniel Pink","Alex Pentland","Eric Schmidt","Neil deGrasse Tyson","Bill Gates"};
+    public String[] names = new String[]{"Daniel Pink","Alex Pentland","Eric Schmidt","Donald Trump","Neil deGrasse Tyson","Bill Gates"};
 
     @FXML
     private JFXTextField name1;
