2270240323a14a1f7cbc28c29eca9b05f65ad8d3
Michal Jarski <michal@jarski.eu>
2021-01-14 14:19:35 +0100

    Allow running 1v1 2v2 3v3 using arguments

--- src/main/java/game/tests/TestingBots.java
+++ src/main/java/game/tests/TestingBots.java
@@ -29,7 +29,7 @@ public class TestingBots {
     protected static final ArrayList<String> playerNames = new ArrayList<>(Arrays.asList("Henry", "Melissa", "Frank", "Jessica", "Dave", "Paola"));
     protected static final Color[] pieceColors = new Color[]{Color.cyan, Color.red, Color.green, Color.gray, Color.black, Color.blue};
     protected static String[] players;
-    protected static int[] wins;
+    protected static int[] wins = new int[6];
     protected static int ties;
     private static final int LOGICAL_THREADS = Runtime.getRuntime().availableProcessors();
     protected static int current_threads = 0;
@@ -60,12 +60,14 @@ public class TestingBots {
     }*/
 
     public static List<Integer> occupied;
-    
+
     public static void oldMain(String[] args) {
     	long startTime = System.nanoTime();
+        System.out.println(Arrays.asList(args));
+    	players=args;
         GameSetup gameSetup = new GameSetup();
-        for (int i = 0; i < players.length; i++) {
-            gameSetup.addPlayer(str2p(players[i]), playerNames.get(i), pieceColors[i]);
+        for (int i = 0; i < args.length; i++) {
+            gameSetup.addPlayer(str2p(args[i]), playerNames.get(i), pieceColors[i]);
         }
         Board gameBoard = gameSetup.getBoard();
         occupied = new ArrayList<>();
@@ -102,13 +104,16 @@ public class TestingBots {
         }
         long finishTime = System.nanoTime();
         System.out.println("GAMES WON:");
-        for(int i=0; i<players.length;i++){
+        for(int i=0; i<args.length;i++){
             System.out.println(gameBoard.getPlayer(i).getTypeName()+": "+wins[i]);
         }
         System.out.println("GAMES TIED: "+ties);
-        System.out.println("Time taken:"+(finishTime-startTime)+" nanoseconds");
+        System.out.println("Time taken:"+(double) (finishTime-startTime)/1e9+" s");
+        // System.out.println("Average time to create board:"+MTRunnable.timeTaken/MTRunnable.runs/1e9);
+        // System.out.println("Time taken to create all boards:"+MTRunnable.timeTaken/1e9);
     }
-    
+
+    /* VERSION BY PASCAL
     public static void main(String[] args) throws InterruptedException {
     	MTRunnable.data_name = "new_testing";
     	MTRunnable.max_turns = 2000;
@@ -126,6 +131,45 @@ public class TestingBots {
         wins = new int[players.length];
         ties = 0;
         oldMain(args);
+    }*/
+
+    /**
+     * Main method for running tests between two selected bots
+     * @param args in the form of player1type player2type boolean1v1 boolean2v2 boolean3v3
+     * @throws InterruptedException
+     */
+    public static void main (String[] args) throws InterruptedException{
+        MTRunnable.data_name = "new_testing";
+        MTRunnable.max_turns = 2000;
+        tests_completed = 0;
+        TESTS_NUMBER = 16;
+        boolean test1vs1 = Boolean.parseBoolean(args[2]), test2vs2 = Boolean.parseBoolean(args[3]), test3vs3 = Boolean.parseBoolean(args[4]);
+        if(test1vs1) run1vs1(args[0], args[1]);
+        if(test2vs2) run2vs2(args[0], args[1]);
+        if(test3vs3) run3vs3(args[0], args[1]);
+    }
+
+    private static void run1vs1(String p1, String p2){
+        oldMain(new String[]{p1,p2});
+        oldMain(new String[]{p2,p1});
+    }
+
+    private static void run2vs2(String p1, String p2){
+        oldMain(new String[]{p1,p2,p2,p1});
+        oldMain(new String[]{p2,p1,p1,p2});
+        oldMain(new String[]{p1,p2,p1,p2});
+        oldMain(new String[]{p2,p1,p1,p2});
+    }
+    private static void run3vs3(String p1, String p2){
+        cover3v3(p2, p1);
+        cover3v3(p1, p2);
+    }
+
+    private static void cover3v3(String p1, String p2) {
+        oldMain(new String[]{p1,p2,p1,p2,p1,p2});
+        oldMain(new String[]{p1,p2,p1,p2,p2,p1});
+        oldMain(new String[]{p1,p2,p2,p1,p1,p2});
+        oldMain(new String[]{p1,p2,p2,p1,p2,p1});
     }
 
     protected static Player str2p(String playerAsString){
